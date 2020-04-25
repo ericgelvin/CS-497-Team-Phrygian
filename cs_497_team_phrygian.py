0ae61f55184d5e3bb082b696f06734fb424aa0b0
ericgelvin <64245953+ericgelvin@users.noreply.github.com>
2020-04-24 23:06:29 -0700

    read kaggle data

--- cs_497_team_phrygian.py
+++ cs_497_team_phrygian.py
@@ -2,48 +2,31 @@
 from flask import Flask, jsonify
 from flask import request
 from sklearn.linear_model import LogisticRegression
+import numpy as np
+import pandas as pd
 import joblib
-
-# https://www.kaggle.com/washingtonpost/police-shootings
-# TODO: parse kaggle data and split into sample data
-
 
 app = Flask(__name__)
 
-samples = [
-    {
-        'name': 'john smith',
-        'date': '2020-01-03,
-        'manner_of_death': 4,
-        'armed': 0,
-        'age': 25,
-        'gender': 1,
-        'race': 3,	
-        'city': 'san diego',
-        'signs_of_mental_illness': 0,
-        'threat_level': 1,
-        'flee': 0,
-        'body_camera': 0
-    }
-]
-
 @app.route('/data/sample', methods=['GET'])
 def get_samples():
-    return jsonify({'samples': samples})
+    # https://www.kaggle.com/washingtonpost/police-shootings
+    import zipfile
+    with zipfile.ZipFile("../input/data/police-shootings.zip","r") as z:
+        z.extractall(".")   
+    from subprocess import check_output
+    print(check_output(["ls", "police-shootings"]).decode("utf8"))
+    d = pd.read_csv("police-shootings/police-shootings.csv")
+    d.head()
+    return jsonify({'samples': d})
 
 @app.route('/predict/sample', methods=['POST'])
 def predict():
     if not request.json:
         abort(400)
     
-    sample = {
-        }
-
     # TODO: get finalized model or build here
-
-    #load the saved model
     model1 = joblib.load('./finalized_model.pkl')
-    #predicting
     y_pred = model1.predict(X_test)
     return jsonify({'result': y_pred})
 

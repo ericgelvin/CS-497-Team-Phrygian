e2daf60f9087718db09353d38ef3a6db0e1c8e8e
Mario Rodriguez <mario.rodriguez.0000@gmail.com>
2020-05-12 21:31:11 -0700

    Client added to do the part of adding the 1389 elements to pass to the model

--- Client.py
+++ Client.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import json
 
 # '{"manner_of_death":0,"armed":0,"age":35,"gender":0,"race":1,"city":2,
 #"state":2,"signs_of_mental_illness":1,"threat_level":1,"flee":0,"body_camera":1}'
@@ -7,7 +8,7 @@ file = open('./features.dat', 'rb')
 features = pickle.load(file)
 file.close
 
-command = "curl -i -H \"Content-Type: application/json\" -X POST -d \'"
+command = "curl -i -H \'Content-Type: application/json\" -X POST -d \""
 
 
 testDat = {}
@@ -25,5 +26,10 @@ testDat['attack'] = 1
 testDat['Not fleeing'] = 1
 testDat['False'] =1
 
-command = command + str(testDat) + "\' http://localhost:5000/predict/sample"
+
+jsonData = json.dumps(testDat)
+command = command + jsonData + "\' http://localhost:5000/predict/sample"
+
+print(command)
+
 os.system(command)
